df6fb088f0bab3abd0594d18d684c29d394d5f4b
abdulrahman2202 <abdulrahman2.ar44@gmail.com>
2023-06-09 13:14:29 +0530

    new

---
+++ landingpage.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector('#singup-btn');
+const form = document.querySelector(".wrapper");
+const closerbtn = document.querySelector(".close-wrapper");
+
+btn.addeventlistener("click", () => {
+     form.style.display = "block";
+});
+
+cloudbtn.addeventlistener('click', () => {
+     form.style.display = "none";
+});
+
+// const namepattern = / * 
